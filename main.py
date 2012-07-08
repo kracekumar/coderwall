8932333e7414e189190e77db61244fcdeca69845
kracekumar <kracethekingmaker@gmail.com>
2012-07-08 12:23:58 +0530

    started using cement

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #! /usr/bin/env python
 #! -*- coding: utf-8 -*-
 
-import requests
-import ujson
+try:
+    import requests
+except ImportError:
+    raise ImportError("Unable to import requests. Try pip install requests")
+try:
+    import ujson
+except ImportError:
+    raise ImportError("Unable to import ujson. Try pip install ujson")
+try:
+    from cement.core import foundation
+except ImportError:
+    raise ImportError("Unable to import cement. Try pip install cement")
 
 
 class CoderWall(object):
@@ -20,7 +30,7 @@ class CoderWall(object):
     def fetch_details(self):
         try:
             r = requests.get('/'.join([CoderWall.BASE_URL, self.username]) + '.json')
-            return r.json if r.ok is None
+            return r.json if r.ok else None
         except requests.ConnectionError:
             raise requests.ConnectionError
         except requests.HTTPError:
@@ -35,5 +45,15 @@ class CoderWall(object):
     def error(self):
         print("User: %s not found" % (self.username))
 
+
+def main():
+    app = foundation.CementApp("CoderWall")
+    try:
+        app.setup()
+        app.run()
+    finally:
+        app.close()
+
 if __name__ == "__main__":
-    CoderWall('kracekumar')
+    main()
+
